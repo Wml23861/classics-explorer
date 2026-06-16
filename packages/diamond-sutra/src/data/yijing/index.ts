780b1ea9ac4bc15b76aff5454957d2300090f62e
王明龙 <wangminglong_140@boe.com.cn>
2026-06-16 17:29:47 +0800

    feat: 48井卦——54卦通过

--- packages/diamond-sutra/src/data/yijing/index.ts
+++ packages/diamond-sutra/src/data/yijing/index.ts
@@ -13,6 +13,7 @@ import h35 from './35'
 import h36 from './36'
 import h46 from './46'
 import h47 from './47'
+import h48 from './48'
 import h56 from './56'; import h57 from './57'; import h58 from './58'
 import h05 from './05'
 import h06 from './06'
@@ -48,7 +49,7 @@ export const hexagrams: Hexagram[] = [
   h11, h12, h13, h14, h15, h16, h17, h18, h19,
   h20, h21, h22, h23, h24, h25, h26, h27, h28, h29,
   h30, h31, h32, h33, h34, h35, h36, h37, h38, h39,
-  h40, h41, h42, h43, h44, h45, h46, h47, h56, h57, h58,
+  h40, h41, h42, h43, h44, h45, h46, h47, h48, h56, h57, h58,
   h61, h62, h64,
 ]
 
