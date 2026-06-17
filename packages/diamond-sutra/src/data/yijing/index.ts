b5019f4307b2ba84c24ec03a69ac5546baa8a6e8
王明龙 <wangminglong_140@boe.com.cn>
2026-06-17 09:13:30 +0800

    feat: 49革卦/50鼎卦通过——56卦构建0错误

--- packages/diamond-sutra/src/data/yijing/index.ts
+++ packages/diamond-sutra/src/data/yijing/index.ts
@@ -14,6 +14,8 @@ import h36 from './36'
 import h46 from './46'
 import h47 from './47'
 import h48 from './48'
+import h49 from './49'
+import h50 from './50'
 import h56 from './56'; import h57 from './57'; import h58 from './58'
 import h05 from './05'
 import h06 from './06'
@@ -49,7 +51,7 @@ export const hexagrams: Hexagram[] = [
   h11, h12, h13, h14, h15, h16, h17, h18, h19,
   h20, h21, h22, h23, h24, h25, h26, h27, h28, h29,
   h30, h31, h32, h33, h34, h35, h36, h37, h38, h39,
-  h40, h41, h42, h43, h44, h45, h46, h47, h48, h56, h57, h58,
+  h40, h41, h42, h43, h44, h45, h46, h47, h48, h49, h50, h56, h57, h58,
   h61, h62, h64,
 ]
 
